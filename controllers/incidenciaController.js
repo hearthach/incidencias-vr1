3e562ad1a98af6be9c6ac4bfbf232d5dbb095a19
hearthach <mplandauro@gmail.com>
2024-01-05 00:51:42 -0500

    probando

--- controllers/incidenciaController.js
+++ controllers/incidenciaController.js
@@ -30,51 +30,51 @@ exports.crearIncidenciaConRemitente = (req, res) => {
 };
 
 function crearIncidencia(datosIncidencia, res, requestBody) {
-    Incidencia.crear(datosIncidencia, (errorIncidencia, resultadoIncidencia) => {
-        if (errorIncidencia) {
-            return res.status(500).send({ mensaje: "Error al crear incidencia", error: errorIncidencia });
-        }
-      
-        // Enviar correo electrónico después de crear la incidencia
-        const emailOptions = {
-            from: process.env.EMAIL_FROM, // Configura estas variables en tu archivo .env
-            to: process.env.EMAIL_TO,
-            subject: 'Nueva Incidencia Registrada',
-            text: `Se ha registrado una nueva incidencia: ${requestBody.incidencia.descripcion}` //verificars ruta es correcta y existe
-        };
-      
-        // Llama a la función sendEmail de mailer.js
-        sendEmail(emailOptions);
-      
-        res.status(201).send({
-            mensaje: "Incidencia creada exitosamente",
-            idIncidencia: resultadoIncidencia.insertId,
-        });
+  Incidencia.crear(datosIncidencia, (errorIncidencia, resultadoIncidencia) => {
+    if (errorIncidencia) {
+      return res.status(500).send({ mensaje: "Error al crear incidencia", error: errorIncidencia });
+    }
+
+    // Enviar correo electrónico después de crear la incidencia
+    const emailOptions = {
+      from: process.env.EMAIL_FROM, // Configura estas variables en tu archivo .env
+      to: process.env.EMAIL_TO,
+      subject: 'Nueva Incidencia Registrada',
+      text: `Se ha registrado una nueva incidencia: ${requestBody.incidencia.descripcion}` //verificars ruta es correcta y existe
+    };
+
+    // Llama a la función sendEmail de mailer.js
+    sendEmail(emailOptions);
+
+    res.status(201).send({
+      mensaje: "Incidencia creada exitosamente",
+      idIncidencia: resultadoIncidencia.insertId,
     });
+  });
 }
 
 exports.crearIncidencia = (req, res) => {
-    Incidencia.crear(req.body, (error, resultado) => {
-        if (error) {
-            return res.status(500).send({ mensaje: "Error al crear la incidencia", error });
-        }
+  Incidencia.crear(req.body, (error, resultado) => {
+    if (error) {
+      return res.status(500).send({ mensaje: "Error al crear la incidencia", error });
+    }
 
-        // Enviar correo electrónico después de crear la incidencia
-        const emailOptions = {
-            from: process.env.EMAIL_FROM, 
-            to: process.env.EMAIL_TO,
-            subject: 'Nueva Incidencia Registrada',
-            text: `Se ha registrado una nueva incidencia: ${req.body.descripcion}` // Averificar si la descripción existe en req.body
-        };
+    // Enviar correo electrónico después de crear la incidencia
+    const emailOptions = {
+      from: process.env.EMAIL_FROM,
+      to: process.env.EMAIL_TO,
+      subject: 'Nueva Incidencia Registrada',
+      text: `Se ha registrado una nueva incidencia: ${req.body.descripcion}` // Averificar si la descripción existe en req.body
+    };
 
-        // Llama a la función sendEmail de mailer.js
-        sendEmail(emailOptions);
+    // Llama a la función sendEmail de mailer.js
+    sendEmail(emailOptions);
 
-        res.status(201).send({
-            mensaje: "Incidencia creada exitosamente",
-            idIncidencia: resultado.insertId,
-        });
+    res.status(201).send({
+      mensaje: "Incidencia creada exitosamente",
+      idIncidencia: resultado.insertId,
     });
+  });
 };
 
 exports.obtenerIncidencias = (req, res) => {
